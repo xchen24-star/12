afcff78f5babcec2f1f1cf7d25ab446e5a47c486
xchen24-star <144645283+xchen24-star@users.noreply.github.com>
2023-10-11 18:36:11 +0000

    page 1

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Welcome to SvelteKit</h1>
+<h2 class="heading">hi</h2>
+<p1>hi</p1>
+
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    .heading {
+        color:blue;
+    }
+</style>
